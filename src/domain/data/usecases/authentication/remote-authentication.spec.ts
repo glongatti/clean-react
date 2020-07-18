e19732045f2873fdb4a91257780c426e03521a85
Gabriel <longattigabriel@gmail.com>
2020-07-17 21:03:06 -0300

    chore: add faker

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -5,15 +5,16 @@ type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
+
 const makeSut = (url: string = 'anu_url'): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
-
   return {
     sut,
     httpPostClientSpy
   }
 }
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpClient with correct URL', async () => {
     const url = 'other_url'
